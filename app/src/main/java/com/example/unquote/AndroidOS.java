601c7a6b8d075dfb1cc128ca11b5a9aca29584c7
James Lloyd <jameslloyd2007@hotmail.co.uk>
2023-10-20 12:12:34 +0100

    add toString method to print a simple draft display of each question to test values have been entered successfully

--- app/src/main/java/com/example/unquote/AndroidOS.java
+++ app/src/main/java/com/example/unquote/AndroidOS.java
@@ -5,14 +5,19 @@ public class AndroidOS {
         System.out.println("AndroidOS Booting Up...");
     }
 
-
-
     public void runQuizApp() {
         // Create three questions here
 Question questionOne = new Question(921238, "How tall is the Eiffel tower?", "1024 ft", "1063 ft", "1124 ft", "1163 ft", 1, -1);
 Question questionTwo = new Question(107343, "Who invented the computer algorithm?", "Charles Babbage", "John Carmack", "Alan Turing", "Ada Lovelace",3, -1);
 Question questionThree = new Question(748294, "What is the name for the patch of skin found on your elbow?", "Elbow Skin", "Fascia Elbora", "Wenis", "Todd", 2, - 1);
         // System.out.println("Question 1: "" + ...)
+        System.out.println(toString(questionOne));
+        System.out.println(toString(questionTwo));
+        System.out.println(toString(questionThree));
+    }
+
+    public String toString(Question question){
+return (question.imageId)+" "+(question.questionText)+" "+(question.answer0)+" "+(question.answer1)+" "+(question.answer2)+" "+(question.answer3)+" "+(question.correctAnswer)+" "+(question.playerAnswer);
     }
 
     public static void main(String[] args) {
