3aeb79e4a36665abee396ea4ec3d210cc84f2f43
James Lloyd <jameslloyd2007@hotmail.co.uk>
2023-10-20 11:21:21 +0100

    define initial variables and create constructor

--- app/src/main/java/com/example/unquote/Question.java
+++ app/src/main/java/com/example/unquote/Question.java
@@ -1,4 +1,25 @@
 package com.example.unquote;
 
 public class Question {
+int correctAnswer;
+int playerAnswer;
+int imageId;
+String questionText;
+String answer0;
+String answer1;
+String answer2;
+String answer3;
+
+public Question(int imageIdentifier, String questionString, String answerZero, String answerOne, String answerTwo, String answerThree, int correctAnswerIndex){
+
+imageId = imageIdentifier;
+questionText = questionString;
+answer0 = answerZero;
+answer1 = answerOne;
+answer2 = answerTwo;
+answer3 = answerThree;
+correctAnswer = correctAnswerIndex;
+
 }
+}
+
