72af1b41688a2e3fa5a6564a097edd668a3f67a8
James Lloyd <jameslloyd2007@hotmail.co.uk>
2023-10-26 12:26:06 +0100

    rename main.java as main-old.java, copy over new main code from codecademy.com add interger member variables (to do part 1) add Arraylist variable questions, add beginning of startNewGame method

--- app/src/main/java/com/example/unquote/MainActivity.java
+++ app/src/main/java/com/example/unquote/MainActivity.java
@@ -2,25 +2,27 @@ package com.example.unquote;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+//import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.example.unquote.databinding.ActivityMainBinding;
+import java.util.ArrayList;
+//import androidx.navigation.NavController;
+//import androidx.navigation.Navigation;
+//import androidx.navigation.ui.AppBarConfiguration;
+//import androidx.navigation.ui.NavigationUI;
+
+//import com.example.unquote.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
-
+//    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //This has been added to make a blank screen rather than one with standard navigation buttons at the bottom
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        /*        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
@@ -32,7 +34,30 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+        */
+
     }
+
+    // TODO #1: add integer member variables here
+
+    int currentQuestionIndex;
+    int totalCorrect;
+    int totalQuestions;
+
+    // TODO #2: add ArrayList member variable here
+
+    ArrayList<Question> questions;
+
+    // TODO #3 add startNewGame() here
+
+    public void startNewGame(){
+    }
+
+    // TODO #4 add chooseNewQuestion() here
+
+    // TODO #5 add getCurrentQuestion() here
+
+    // TODO #6 add onAnswerSubmission() here
 
     public int generateRandomNumber(int max){
 double randomNumber = Math.random();
@@ -45,7 +70,7 @@ public String getGameOverMessage(int totalCorrect, int totalQuestions) {
     if (totalCorrect == totalQuestions) {
         return "You got all " + (totalQuestions) + " right! You won!";
     } else {
-        return "You got " + totalCorrect + " out of " + totalQuestions + ". Better luck next time!";
+        return "You got " + totalCorrect + "correct out of " + totalQuestions + ". Better luck next time!";
     }
 }
 
