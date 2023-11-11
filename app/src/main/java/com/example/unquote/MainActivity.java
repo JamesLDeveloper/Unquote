06c0d3c7ce1bb9d774771273e145f3d2959f8d43
James Lloyd <jameslloyd2007@hotmail.co.uk>
2023-11-11 12:08:42 +0000

    set up member variables

--- app/src/main/java/com/example/unquote/MainActivity.java
+++ app/src/main/java/com/example/unquote/MainActivity.java
@@ -24,6 +24,25 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    int currentQuestionIndex;
+    int totalCorrect;
+    int totalQuestions;
+
+    // TODO #2: add ArrayList member variable here
+
+    ArrayList<Question> questions;
+
+    // TODO 3-A: Declare View member variables
+
+ImageView questionImageView;
+TextView questionTextView;
+TextView questionsRemainingTextView;
+Button answer0Button;
+Button answer1Button;
+Button answer2Button;
+Button answer3Button;
+Button submitButton;
+
 
 //    private ActivityMainBinding binding;
     @Override
@@ -56,6 +75,17 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO 3-B: assign View member variables
 
+        questionImageView = findViewById(R.id.iv_main_question_image);
+        questionTextView = findViewById(R.id.tv_main_question_title);
+        questionsRemainingTextView = findViewById(R.id.tv_main_questions_remaining);
+        answer0Button = findViewById(R.id.btn_main_answer_0);
+        answer1Button = findViewById(R.id.btn_main_answer_1);
+        answer2Button = findViewById(R.id.btn_main_answer_2);
+        answer3Button = findViewById(R.id.btn_main_answer_3);
+        submitButton = findViewById(R.id.btn_main_submit_answer);
+
+
+
         // TODO 4-E: set onClickListener for each answer Button
 
         // TODO 5-A: set onClickListener for the submit answer Button
@@ -68,32 +98,41 @@ public class MainActivity extends AppCompatActivity {
 
     // TODO 3-C: displayQuestionsRemaining(int questionRemaining) {...}
 
+    public void displayQuestionsRemaining(int q){
+
+
+    }
+
     // TODO 4-A: onAnswerSelected(int answerSelected) {...}
 
 
 
     // TODO #1: add integer member variables here
 
-    int currentQuestionIndex;
-    int totalCorrect;
-    int totalQuestions;
 
-    // TODO #2: add ArrayList member variable here
 
-    ArrayList<Question> questions;
 
     // TODO #3 add startNewGame() here
 
     public void startNewGame(){
 
-        Question questionOne = new Question(921238, "How tall is the Eiffel tower?", "1024 ft", "1063 ft", "1124 ft", "1163 ft", 1);
-        Question questionTwo = new Question(107343, "Who invented the computer algorithm?", "Charles Babbage", "John Carmack", "Alan Turing", "Ada Lovelace",3);
-        Question questionThree = new Question(748294, "What is the name for the patch of skin found on your elbow?", "Elbow Skin", "Fascia Elbora", "Wenis", "Todd", 2);
-
+        // TODO 2-H: Provide actual drawables for each of these questions!
         questions = new ArrayList<>();
-        questions.add(questionOne);
-        questions.add(questionTwo);
-        questions.add(questionThree);
+
+
+        Question question0 = new Question(R.drawable.img_quote_0, "Pretty good advice, and perhaps a scientist did say it… Who actually did?", "Albert Einstein", "Isaac Newton", "Rita Mae Brown", "Rosalind Franklin", 2);
+        Question question1 = new Question(R.drawable.img_quote_1, "Was honest Abe honestly quoted? Who authored this pithy bit of wisdom?", "Edward Stieglitz", "Maya Angelou", "Abraham Lincoln", "Ralph Waldo Emerson", 0);
+        Question question2 = new Question(R.drawable.img_quote_2, "Easy advice to read, difficult advice to follow — who actually said it?", "Martin Luther King Jr.", "Mother Teresa", "Fred Rogers", "Oprah Winfrey", 1);
+        Question question3 = new Question(R.drawable.img_quote_3, "Insanely inspiring, insanely incorrect (maybe). Who is the true source of this inspiration?", "Nelson Mandela", "Harriet Tubman", "Mahatma Gandhi", "Nicholas Klein", 3);
+        Question question4 = new Question(R.drawable.img_quote_4, "A peace worth striving for — who actually reminded us of this?", "Malata Yousafzai", "Martin Luther King Jn.", "Liu Xiaobo", "Dalai Lama", 1);
+        Question question5 = new Question(R.drawable.img_quote_5, "Unfortunately, true — but did Marilyn Monroe convey it or did someone else?", "Laurel Thatcher Ulrich", "Eleanor Roosevelt", "Marilyn Monroe", "Queen Victoria", 0);
+
+        questions.add(question0);
+        questions.add(question1);
+        questions.add(question2);
+        questions.add(question3);
+        questions.add(question4);
+        questions.add(question5);
 
         totalCorrect = 0;
         totalQuestions = questions.size();
